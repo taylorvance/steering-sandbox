59bfed663f48623bc525b270fd38741a379339b4
Taylor Vance <tvance989@gmail.com>
2017-11-18 11:41:30 -0600

    Set up watchify script and render game

--- main.js
+++ main.js
@@ -5,17 +5,42 @@ var Red = function(pos, vel) {
 	Vehicle.call(this, pos, vel);
 }
 Red.prototype = new Vehicle;
-Red.prototype.max_speed = 10;
-Red.prototype.max_force = 5;
-Red.prototype.mass = 1;
-Red.prototype.perception = 20;
-Red.prototype.leeway = 2;
 
+// config Red vehicle vars
+Red.prototype.max_speed = 300;
+Red.prototype.max_force = 50;
+Red.prototype.mass = 1;
+Red.prototype.perception = 50;
+Red.prototype.leeway = 10;
 
 var red = new Red;
 //console.log(red);
 
 
+
+
+
+var canvas = document.getElementById('canvas');
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+canvas.height -= 50;
+var ctx = canvas.getContext('2d');
+
+var mouse = new Vector;
+function mousemove(x, y) {
+	mouse.x = x;
+	mouse.y = y;
+}
+
+function render() {
+	ctx.fillStyle = '#def';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.save();
+	ctx.fillStyle = '#c00';
+	ctx.fillRect(red.position.x, red.position.y, 3, 3);
+	ctx.restore();
+}
 
 var FPS = 30;
 var intervalID;
@@ -33,13 +58,14 @@ function tick() {
 	var dt = (now - lastUpdate) / 1000;
 	lastUpdate = now;
 
-	var target = new Vector(230, 420);
-	var force = red.arrive(target);
-	red.apply_force(force);
+	var force = red.arrive(mouse);
 
-	console.log(red.position.distance(target), red.velocity.magnitude());
+	red.apply_force(force, dt);
+
+	render();
 }
 play();
 
 global.play = play;//.hack bc i'm not using browserify standalone (yet?)
 global.pause = pause;//.hack bc i'm not using browserify standalone (yet?)
+global.mousemove = mousemove;//.hack bc i'm not using browserify standalone (yet?)
